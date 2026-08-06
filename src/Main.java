ae6da3c2329d3992b8cd01a6d7346ad80e537aea
Danae_Gatsou <iegatsou@gmail.com>
2026-08-06 11:34:56 +0300

    Day 1 - initial Java setup

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String name = "George";
+        int age = 30;
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old.");
+        age++;
+        System.out.println("Next year I will be " + age + " years old.");
+
+    }
+}
