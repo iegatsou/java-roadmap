95432d90edbc02068c9c38cff33ca408162df00f
Danae_Gatsou <iegatsou@gmail.com>
2026-08-10 17:06:16 +0300

    Day 15 - OOP basics Student

--- src/week03/day15/Student.java
+++ src/week03/day15/Student.java
@@ -1,4 +1,20 @@
 package week03.day15;
 
 public class Student {
+    String name;
+    int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void printInfo() {
+        System.out.println(name + " - " + grade);
+    }
+    public boolean hasPassed(){
+
+            return grade>=50;
+
+    }
 }
--- src/week03/day15/StudentApp.java
+++ src/week03/day15/StudentApp.java
@@ -1,4 +1,15 @@
 package week03.day15;
 
 public class StudentApp {
+    public static void main(String[] args) {
+        Student anna = new Student("Anna", 78);
+        Student john = new Student("John", 45);
+        Student maria = new Student("Maria", 92);
+        anna.printInfo();
+        System.out.println("Passed: " + anna.hasPassed());
+        john.printInfo();
+        System.out.println("Passed: " + john.hasPassed());
+        maria.printInfo();
+        System.out.println("Passed: " + maria.hasPassed());
+    }
 }
